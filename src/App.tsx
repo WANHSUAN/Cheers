ad44242edb1f0ebd57537d3ff441ab6b1c732850
wanhsuanlin <energy2429@gmail.com>
2023-04-19 10:26:40 +0800

    [Modify] Change App.tsx and Index.tsx setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import React from "react";
-import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
-import Question from "./pages/Question/Question";
-import Login from "./pages/Login/Login";
-import Main from "./pages/Main/Main";
-import Bar from "./pages/Bar/Bar";
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
 import {config} from "./config/config";
 import {AuthContextProvider} from "./Context/AuthContext";
-import RecommendationPage from "./pages/Recommendation/Recommendation";
-import Event from "./pages/Event/Event";
-import Member from "./pages/Member/Member";
-import Category from "./pages/Category/Category";
-import Search from "./pages/Search/Search";
+import {Outlet} from "react-router-dom";
+import Header from "./components/Header/Header";
 
 const app = initializeApp(config.firebaseConfig);
 export const db = getFirestore(app);
@@ -21,22 +12,10 @@ export interface IAppProps {}
 
 const App: React.FC<IAppProps> = (props) => {
   return (
-    <BrowserRouter>
-      <AuthContextProvider>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/question" element={<Question />} />
-          <Route path="/recommendation" element={<RecommendationPage />} />
-          <Route path="/main" element={<Main />} />
-          <Route path="/bars/:id" element={<Bar />} />
-          <Route path="/events/:id" element={<Event />} />
-          <Route path="/member" element={<Member />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </AuthContextProvider>
-    </BrowserRouter>
+    <AuthContextProvider>
+      <Header />
+      <Outlet />
+    </AuthContextProvider>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
 import ReactDOM from "react-dom/client";
+import React from "react";
+import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import App from "./App";
-import Header from "./components/Header/Header";
+import Question from "./pages/Question/Question";
+import Login from "./pages/Login/Login";
+import Main from "./pages/Main/Main";
+import Bar from "./pages/Bar/Bar";
+import RecommendationPage from "./pages/Recommendation/Recommendation";
+import Event from "./pages/Event/Event";
+import Member from "./pages/Member/Member";
+import Category from "./pages/Category/Category";
+import Search from "./pages/Search/Search";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <>
-    <Header />
-    <App />
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Login />} />
+        <Route path="/question" element={<Question />} />
+        <Route path="/recommendation" element={<RecommendationPage />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/bars/:id" element={<Bar />} />
+        <Route path="/events/:id" element={<Event />} />
+        <Route path="/member" element={<Member />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
