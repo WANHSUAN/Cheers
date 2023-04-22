458f0c8f19a435f6cc65c0a1f0dc54441a83c189
wanhsuanlin <energy2429@gmail.com>
2023-04-23 00:23:31 +0800

    [Feat] Event Page completed

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -4,35 +4,87 @@ import {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 import {db} from "../../App";
 import {getDoc, doc} from "firebase/firestore";
-import SideMenu from "../../components/SideMenu/SideMenu";
+import {RxDoubleArrowRight} from "react-icons/rx";
 
 const Wrapper = styled.div`
   padding: 10px;
-`;
-
-const MenuButton = styled.button`
-  width: 50px;
-  height: 30px;
+  padding-top: 80px;
+  letter-spacing: 3px;
 `;
 
 const PageImg = styled.img`
   width: 100vw;
-  height: 10%;
+  height: 300px;
+  object-fit: cover;
 `;
 
-const EventTitle = styled.h2``;
+const EventTitle = styled.h2`
+  color: #d19b18;
+  font-size: 40px;
+  margin: 80px 80px 80px 0;
+`;
 
-const EventContent = styled.p``;
+const InnerDiv = styled.div`
+  width: 100%;
+  height: 430px;
+  border: 2px solid white;
+  padding: 10px;
+  margin-top: -25px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+const OuterDiv = styled.div`
+  width: 100%;
+  height: 400px;
+  border: 2px solid white;
+  padding: 10px;
+  margin: 0 auto;
+`;
+
+const EventSection = styled.div`
+  width: 1000px;
+  margin: 0 auto;
+`;
+
+const EventContent = styled.div`
+  width: 80%;
+  color: #fff;
+  font-size: 23px;
+  margin-top: 130px;
+  line-height: 30px;
+`;
 
 // const EventTime = styled.div``;
 
-const BarEnterButton = styled(Link)`
-  text-decoration: none;
+const StyledBarEnterButton = styled.div`
+  width: 1000px;
+  height: 30px;
+  border: none;
+  background-color: rgba(255, 255, 255, 0);
+  cursor: pointer;
+  border: none;
+  font-size: 30px;
+  margin: 80px 0 150px 0;
+  display: flex;
+  justify-content: right;
 `;
 
-const StyledBarEnterButton = styled.button`
-  width: 150px;
-  height: 30px;
+const BarEnterButton = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+`;
+
+const StyledDoubleArrow = styled.button`
+  height: 50px;
+  color: #fff;
+  font-size: 30px;
+  background-color: rgba(255, 255, 255, 0);
+  border: none;
+  text-align: center;
+  cursor: pointer;
+  justify-content: center;
 `;
 
 interface IEvent {
@@ -47,7 +99,6 @@ export interface IEventProps {}
 
 const EventPage: React.FC<IEventProps> = (props: IEventProps) => {
   const [event, setEvent] = useState<IEvent>();
-  const [showMenu, setShowMenu] = useState(false);
 
   const {id} = useParams();
   const eventCollectionRef = id ? doc(db, "events", id) : undefined;
@@ -67,23 +118,27 @@ const EventPage: React.FC<IEventProps> = (props: IEventProps) => {
     return <p>Loading...</p>;
   }
 
-  function handleMenuClick() {
-    setShowMenu(!showMenu);
-  }
-
   return (
     <Wrapper>
-      <MenuButton onClick={handleMenuClick}>Menu</MenuButton>
-      {showMenu && <SideMenu />}
       <PageImg src={event.img} />
-      <EventTitle>{event.bar}</EventTitle>
-      {/* <EventTime>{event.time}</EventTime> */}
-      <EventContent>{event.content}</EventContent>
-      <StyledBarEnterButton>
-        <BarEnterButton to={`/bars/${event.id}`} key={event.id}>
-          Enter Event Bar
-        </BarEnterButton>
-      </StyledBarEnterButton>
+      <EventSection>
+        <EventTitle>{event.bar}</EventTitle>
+        {/* <EventTime>{event.time}</EventTime> */}
+        <OuterDiv>
+          <InnerDiv>
+            <EventContent>{event.content}</EventContent>
+          </InnerDiv>
+        </OuterDiv>
+
+        <StyledBarEnterButton>
+          <BarEnterButton to={`/bars/${event.id}`} key={event.id}>
+            EXPLORE THE BAR
+          </BarEnterButton>
+          <StyledDoubleArrow>
+            <RxDoubleArrowRight />
+          </StyledDoubleArrow>
+        </StyledBarEnterButton>
+      </EventSection>
     </Wrapper>
   );
 };
